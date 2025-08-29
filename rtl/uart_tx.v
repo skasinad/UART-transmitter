e425eb46d28e2101c36d3acbe72853d4d2e38a9a
Satvik Kasinadhuni <skasinad@vl965-172-31-109-247.wireless.umass.edu>
2025-08-29 10:59:58 -0400

    40% done

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -1,7 +1,7 @@
 module uart_tx #(
-    parameter CLK_HZ = 50_000_000, BAUD_RATE = 115_200
-    //CLK_HZ for the frequency of the system's clock in hertz
-    //BAUD_RATE is the bit rate of the UART line 
+    parameter clk_hz = 50_000_000, baud_rate = 115_200
+    //clk_hz for the frequency of the system's clock in hertz
+    //baud_rate is the bit rate of the UART line 
 )(
     input wire clk; 
     input wire rst; 
@@ -11,5 +11,16 @@ module uart_tx #(
     output reg tx_ready; 
     output reg txd;
 );
-localparam integer CLKS_PER_BIT = CLK_HZ/BAUD_RATE; //CLKS_PER_BIT is the number of clock cyclers per UART bit, the relation between the clock frequency and the bitrate
+localparam integer clks_per_bit = clk_hz/baud_rate; //clks_per_bit is the number of clock cycles per UART bit, the relation between the clock frequency and the bitrate
+/* 4 states in the UART line
+1. IDLE 
+2. START
+3. DATA
+4. STOP
+*/
+localparam [1:0] idle_state = 2'b00, start_state = 2'b01, data_state = 2'b10, stop_state = 2'b11;
+reg [1:0] current_state;  
+
+localparam integer timer_width = (clks_per_bit <= 2) ? 1: $clog2(clks_per_bit);
+
 endmodule   
